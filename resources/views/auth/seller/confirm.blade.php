7981623036f983d6c8f02cacb893752ccb0fd113
teyugn901 <thaonguyet02vn@gmail.com>
2023-12-25 17:54:55 +0700

    add templete

--- resources/views/auth/seller/confirm.blade.php
+++ resources/views/auth/seller/confirm.blade.php
@@ -7,26 +7,103 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Create Shop</title>
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
-
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/create-shop-styles.css') }}">
 </head>
 
-<body>
-    @if (session('username'))
-        @php
-            $shopProfile = \App\Models\shopProfile::where('username', session('username'))->first();
-        @endphp
-        <div class="image">
-            <img src="{{ $shopProfile->avt }}" class="img-circle elevation-2" alt="Shop Image"
-                style="width: 50px; height: 50px">
+<div class="body">
+   
+    <div class="main">
+        <div class="container">
+            <div class="form-container">
+                <div class="title">Shop Profile</div>
+                <form method="POST" action="" enctype="multipart/form-data">
+                    @csrf
+                    <div class="form-group">
+                        <label class = "form__label" for="">Status:</label>
+
+                        <h3> Đang chờ duyệt</h3>
+                    </div>
+                    <div class="form-group">
+                        <label class = "form__label" for="">Shop's name:</label>
+                        @if (session('username'))
+                            @php
+                                $shopProfile = \App\Models\shopProfile::where('username', session('username'))->first();
+                            @endphp
+                            <div class="info">
+                                <h6>{{ $shopProfile->name_shop }}</h6>
+                            </div>                           
+                        @endif
+                    </div>
+                    <div class="form-group">
+                        <label class = "form__label" for="">Description:</label>
+
+                        @if (session('username'))
+                            @php
+                                $shopProfile = \App\Models\shopProfile::where('username', session('username'))->first();
+                            @endphp
+                            <div class="info">
+                                <h6>{{ $shopProfile->description }}</h6>
+                            </div>                            
+                        @endif
+                    </div>
+                    <div class="form-group">
+                        <label class = "form__label" for="">Cover Image:</label>
+
+                        @if (session('username'))
+                            @php
+                                $shopProfile = \App\Models\shopProfile::where('username', session('username'))->first();
+                            @endphp
+                            <div class="image">
+                                <img src="{{ $shopProfile->avt }}" class="img-circle elevation-2" alt="Shop Image"
+                                style="width: 150px; height: 150px">
+                            </div>                        
+                        @endif
+                    </div>
+                    <div class="form-group">
+                        <label class = "form__label" for="">Shop's avatar:</label>
+
+                        @if (session('username'))
+                            @php
+                                $shopProfile = \App\Models\shopProfile::where('username', session('username'))->first();
+                            @endphp
+                            <div class="image">
+                                <img src="{{ $shopProfile->cover_image }}" class="img-circle elevation-2" alt="Shop Image"
+                                    style="width: 150px; height: 150px">
+                            </div>                        
+                        @endif
+                    </div>
+                    <div class="form-group">
+                        <label class = "form__label" for="">Shop's name:</label>
+                        @if (session('username'))
+                            @php
+                                $shopProfile = \App\Models\shopProfile::where('username', session('username'))->first();
+                            @endphp
+                            <div class="info">
+                                <h6>{{ $shopProfile->name_shop }}</h6>
+                            </div>                           
+                        @endif
+                    </div>
+                    <div class="form-group">
+                        <label class = "form__label" for="">Description:</label>
+
+                        @if (session('username'))
+                            @php
+                                $shopProfile = \App\Models\shopProfile::where('username', session('username'))->first();
+                            @endphp
+                            <div class="info">
+                                <h6>{{ $shopProfile->description }}</h6>
+                            </div>                            
+                        @endif
+                    </div>
+                   
+
+                    <a href="{{ route('logout') }}">đăng xuất</a>
+                    
+                </form>
+            </div>
         </div>
-        <div class="info">
-            <h6>{{ $shopProfile->name_shop }}</h6>
-        </div>
-        
-    <h1>Chờ xác nhận</h1>
-    @endif
-    <a href="{{ route('logout') }}">đăng xuất</a>
-</body>
+    </div>
+
+</div>
 
 </html>
